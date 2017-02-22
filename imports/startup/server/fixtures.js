c72c725db6e091471776d79bb005d7d7b311ff84
Stephen Goudie <sgoudie@gmail.com>
2017-02-22 21:13:13 +1100

    Add fixtures for clients

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,24 +1,24 @@
-import { Meteor } from 'meteor/meteor';
-import { Roles } from 'meteor/alanning:roles';
-import { Accounts } from 'meteor/accounts-base';
+import {Meteor} from 'meteor/meteor';
+import {Roles} from 'meteor/alanning:roles';
+import {Accounts} from 'meteor/accounts-base';
 // Custom Collections
-import { Clients } from '../../api/clients/clients.js';
+import {Clients} from '../../api/clients/clients.js';
 
 if (!Meteor.isProduction) {
   const users = [{
     email: 'admin@admin.com',
     password: 'password',
     profile: {
-      name: { first: 'Carl', last: 'Winslow' },
+      name: {first: 'Carl', last: 'Winslow'},
     },
     roles: ['admin'],
   }];
 
-  users.forEach(({ email, password, profile, roles }) => {
-    const userExists = Meteor.users.findOne({ 'emails.address': email });
+  users.forEach(({email, password, profile, roles}) => {
+    const userExists = Meteor.users.findOne({'emails.address': email});
 
     if (!userExists) {
-      const userId = Accounts.createUser({ email, password, profile });
+      const userId = Accounts.createUser({email, password, profile});
       Roles.addUsersToRoles(userId, roles);
     }
   });
@@ -41,23 +41,72 @@ if (!Meteor.isProduction) {
     image: 'https://cdn1.iconfinder.com/data/icons/freeline/32/account_friend_human_man_member_person_profile_user_users-512.png'
   }, {
     title: 'Tabarta circtuit re-program required',
-    description: 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.',
+    description: "Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It's also called placeholder (or filler) text. It's a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the ",
     height: '180',
     weight: '50',
     sex: 'female',
     image: 'https://cdn1.iconfinder.com/data/icons/freeline/32/account_friend_human_man_member_person_profile_user_users-512.png'
   }, {
     title: 'Skin fold appointment by a qualified practisioner',
-    description: "Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.",
+    description: "Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It's also called placeholder (or filler) text. It's a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the ",
     height: '170',
     weight: '60',
     sex: 'female',
     image: 'https://cdn1.iconfinder.com/data/icons/freeline/32/account_friend_human_man_member_person_profile_user_users-512.png'
-
-  }];
+  }, {
+    title: 'I had a motorbike accident and have lost substantial body muscle, can anyone assist me post physio therapy regain my preivous composure?',
+    description: "Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It's also called placeholder (or filler) text. It's a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the ",
+    height: '170',
+    weight: '60',
+    sex: 'female',
+    image: 'https://cdn1.iconfinder.com/data/icons/freeline/32/account_friend_human_man_member_person_profile_user_users-512.png'
+  }, {
+    title: 'I just had a baby and I would like to imporve my fitness again and loose some weight post child birth ',
+    description: "Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It's also called placeholder (or filler) text. It's a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the ",
+    height: '170',
+    weight: '60',
+    sex: 'female',
+    image: 'https://cdn1.iconfinder.com/data/icons/freeline/32/account_friend_human_man_member_person_profile_user_users-512.png'
+  }, {
+    title: 'I need to be able to increase my flexibility after working in a 9-5 office for 10 years without exercise.',
+    description: "Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It's also called placeholder (or filler) text. It's a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the ",
+    height: '170',
+    weight: '60',
+    sex: 'female',
+    image: 'https://cdn1.iconfinder.com/data/icons/freeline/32/account_friend_human_man_member_person_profile_user_users-512.png'
+  }, {
+    title: 'help people to get fit and treat injuries all in the one place.',
+    description: "Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It's also called placeholder (or filler) text. It's a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the ",
+    height: '170',
+    weight: '60',
+    sex: 'female',
+    image: 'https://cdn1.iconfinder.com/data/icons/freeline/32/account_friend_human_man_member_person_profile_user_users-512.png'
+  }, {
+    title: 'Skin fold appointment by a qualified practisioner',
+    description: "Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It's also called placeholder (or filler) text. It's a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the ",
+    height: '170',
+    weight: '60',
+    sex: 'female',
+    image: 'https://cdn1.iconfinder.com/data/icons/freeline/32/account_friend_human_man_member_person_profile_user_users-512.png'
+  }, {
+    title: 'Kick PT offers a friendly environment where everyone can get fit while having fun participating in boxing',
+    description: "Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It's also called placeholder (or filler) text. It's a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the ",
+    height: '170',
+    weight: '60',
+    sex: 'female',
+    image: 'https://cdn1.iconfinder.com/data/icons/freeline/32/account_friend_human_man_member_person_profile_user_users-512.png'
+  }, {
+    title: 'Toughen Up is a gym based in Altona focused on boxing and mixed martial arts.',
+    description: "Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It's also called placeholder (or filler) text. It's a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the ",
+    height: '170',
+    weight: '60',
+    sex: 'female',
+    image: 'https://cdn1.iconfinder.com/data/icons/freeline/32/account_friend_human_man_member_person_profile_user_users-512.png'
+  }
+  ];
 
   clients.forEach((client) => {
-    const clientExists = Clients.findOne({ title: client.title });
+    const clientExists = Clients.findOne({title: client.title});
     if (!clientExists) Clients.insert(client);
   });
 
