5e8d340f296c8bb34c31ab085d39c7c31c6ca5ad
Stephen Goudie <sgoudie@gmail.com>
2018-10-09 18:20:27 +1100

    gym-5: Add inbox to menu navigation

--- imports/ui/components/AuthenticatedNavigation.js
+++ imports/ui/components/AuthenticatedNavigation.js
@@ -15,29 +15,30 @@ const userName = () => {
 const AuthenticatedNavigation = () => (
   <div>
     <Nav pullLeft>
-      <NavDropdown eventKey={ 1 } title="Jobs" id="basic-nav-dropdown">
-        <MenuItem eventKey={ 2.3 } href="/jobs">Find Jobs</MenuItem>
-        <MenuItem eventKey={ 1.1 } href="/client/jobs">My Jobs</MenuItem>
-        <MenuItem eventKey={ 2.2 } href="/client/new/job">Post a Job</MenuItem>
+      <NavDropdown eventKey={1} title="Jobs" id="basic-nav-dropdown">
+        <MenuItem eventKey={2.3} href="/jobs">Find Jobs</MenuItem>
+        <MenuItem eventKey={1.1} href="/client/jobs">My Jobs</MenuItem>
+        <MenuItem eventKey={2.2} href="/client/new/job">Post a Job</MenuItem>
       </NavDropdown>
     </Nav>
     <Nav pullLeft>
-      <NavDropdown eventKey={ 1 } title="Trainers" id="basic-nav-dropdown">
-        <MenuItem eventKey={ 1.1 } href="/trainers">Find Trainers</MenuItem>
+      <NavDropdown eventKey={1} title="Trainers" id="basic-nav-dropdown">
+        <MenuItem eventKey={1.1} href="/trainers">Find Trainers</MenuItem>
       </NavDropdown>
     </Nav>
     <Nav pullLeft>
-      <NavDropdown eventKey={ 1 } title="Clients" id="basic-nav-dropdown">
-        <MenuItem eventKey={ 1.1 } href="/clients">Find Clients</MenuItem>
+      <NavDropdown eventKey={1} title="Clients" id="basic-nav-dropdown">
+        <MenuItem eventKey={1.1} href="/clients">Find Clients</MenuItem>
       </NavDropdown>
     </Nav>
     <Nav pullRight>
-      <NavDropdown eventKey={ 3 } title={ userName() } id="basic-nav-dropdown">
-        <MenuItem eventKey={ 3.1 } href="/client/edit/measurement">Edit Measurements</MenuItem>
-        <MenuItem eventKey={ 3.2 } href="/trainer/edit/experience">Edit Experience</MenuItem>
-        <MenuItem eventKey={ 3.3 } href="/trainer/edit/profile">Edit Profile</MenuItem>
-        <MenuItem eventKey={ 3.4 } href="/contactus">Contact Us</MenuItem>
-        <MenuItem eventKey={ 3.5 } onClick={ handleLogout }>Logout</MenuItem>
+      <NavDropdown eventKey={3} title={userName()} id="basic-nav-dropdown">
+        <MenuItem eventKey={3.1} href="/client/edit/measurement">Edit Measurements</MenuItem>
+        <MenuItem eventKey={3.2} href="/trainer/edit/experience">Edit Experience</MenuItem>
+        <MenuItem eventKey={3.3} href="/trainer/edit/profile">Edit Profile</MenuItem>
+        <MenuItem eventKey={3.4} href="/chat-room">Inbox</MenuItem>
+        <MenuItem eventKey={3.4} href="/contactus">Contact Us</MenuItem>
+        <MenuItem eventKey={3.5} onClick={handleLogout}>Logout</MenuItem>
       </NavDropdown>
     </Nav>
   </div>
