4cc08caee19b8b58d74c6abb943b4ebd88ddab69
Stephen Goudie <sgoudie@gmail.com>
2019-04-14 22:42:16 +1000

    gym-1: Centre trainer/new/profile & /trainer/edit/experience, use the same temlplate as to abide by the Don't Repeat Yourself code pattern

--- imports/ui/pages/NewTrainerExperience.js
+++ imports/ui/pages/NewTrainerExperience.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import TrainerExperienceEditor from '../components/TrainerExperienceEditor.js';
+import MyAccountTrainerExperienceEditor from '../components/MyAccountTrainerExperienceEditor.js';
+import {
+  Row,
+  Col
+} from 'react-bootstrap';
+
 
 const NewTrainerExperience = () => (
   <div className="trainerExperienceEditor">
-    <h1 className="page-header">Welcome to Findatrainer…</h1>
-    <TrainerExperienceEditor />
+    <Row>
+      <Col className="col-centered" xs={12} sm={8} md={8}>
+        <h1 className="page-header">Welcome to Findatrainer…</h1>
+      </Col>
+    </Row>
+    <MyAccountTrainerExperienceEditor />
   </div>
 );
 
